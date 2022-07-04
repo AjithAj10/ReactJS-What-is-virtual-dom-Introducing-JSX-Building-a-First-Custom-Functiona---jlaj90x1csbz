32c5fdfd4c66afb0fd1a836466ab6441f55860f0
ajith <ajithraveendranr@gmail.com>
2022-07-04 15:03:08 +0530

    working

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,50 @@
 import React, {Component, useState} from "react";
+import Data from '../data';
 import '../styles/App.css';
 
 const App = () => {
+  const [item,setItem] = useState(Data[0]);
+  const [preVal,setpreVal] = useState(true);
+  const [nextVal,setnextVal] = useState(false);
+
+  function prev(){
+    let temp = Data.indexOf(item) - 1;
+      setItem(() => Data[temp]);
+
+    setnextVal(false);
+    if(temp == 0){
+      setpreVal(true);
+    }
+  }
+
+  function next(){
+    let temp = Data.indexOf(item)+1;
+      setItem(() => Data[temp]);
+
+    setpreVal((previo) => false);
+
+    if(temp == 4){
+      setnextVal(true);
+      
+    }
+  }
+
+  function restart(){
+setItem(() => Data[0]);
+setpreVal(true);
+setnextVal(false);
+  }
+
   return (
-    <></>
+    <>
+    
+    <button data-testid="button-prev" disabled={preVal} onClick={prev}>Prev</button>
+    <button data-testid="button-next" disabled={nextVal}  onClick={next}>Next</button>
+    <button data-testid="button-restart" disabled={preVal}  onClick={restart}>Restart</button>
+
+    <h1 ata-testid="title">{item.title}</h1>
+    <p ata-testid="text">{item.text}</p>
+    </>
   )
 }
 
